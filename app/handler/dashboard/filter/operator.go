02e03af436b47664588dcd86066e76fcb6548ffd
XotoX1337 <frederic.leist@gmail.com>
2025-03-04 10:46:23 +0100

    extracted logic from dashboard/list into function GetActiveMachines

--- app/handler/dashboard/filter/operator.go
+++ app/handler/dashboard/filter/operator.go
@@ -1,92 +1,29 @@
 package filter
 
 import (
-	"encoding/json"
-	"sort"
-	"time"
-
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/log"
-	"github.com/typomedia/patchouli/app/helper"
 	"github.com/typomedia/patchouli/app/store/boltdb"
 	"github.com/typomedia/patchouli/app/structs"
 )
 
 func Operator(c *fiber.Ctx) error {
-	id := c.Params("id")
+	operatorId := c.Params("id")
 	db := boltdb.New()
-
-	err := db.SetBucket("machine")
-	if err != nil {
-		return err
-	}
-
-	machines, _ := db.GetAllByOperatorId(id, "machine")
-
-	Machines := structs.Machines{}
-	for _, v := range machines {
-		machine := structs.Machine{}
-		err = json.Unmarshal(v, &machine)
-		if err != nil {
-			return err
-		}
-
-		lastUpdate, _ := db.GetLastByName(machine.Id, "history")
-
-		update := structs.Update{}
-		err = json.Unmarshal(lastUpdate, &update)
-		if err != nil {
-			log.Error(err)
-		}
-
-		machine.Update = update
-
-		Machines = append(Machines, machine)
-
-	}
-
-	// sort machines by oldest update first
-	sort.Sort(structs.ByDate(Machines))
-
-	err = db.SetBucket("config")
-	if err != nil {
-		return err
-	}
-
-	var config structs.Config
-	db.Get("main", &config, "config")
-
 	defer db.Close()
 
-	interval := config.General.Interval
-	for i := range Machines {
-		currentDate := time.Now()
-
-		if Machines[i].Update.Date == "" {
-			Machines[i].Update.Date = "0000-00-00"
-			Machines[i].Status = "danger"
-			continue
-		}
-
-		Machines[i].Update.Date = helper.UnixToDateString(Machines[i].Update.Date)
-
-		date, err := time.Parse("2006-01-02", Machines[i].Update.Date)
-		if err != nil {
-			log.Error(err)
-		}
-
-		Machines[i].Days = int(currentDate.Sub(date).Hours() / 24)
-		if Machines[i].Days > interval {
-			Machines[i].Status = "danger"
-		} else if Machines[i].Days > interval/3 {
-			Machines[i].Status = "warning"
-		} else {
-			Machines[i].Status = "success"
-		}
-		Machines[i].Days = interval - int(currentDate.Sub(date).Hours()/24)
-
+	machines, err := db.GetActiveMachines()
+	if err != nil {
+		return err
 	}
+
+	var operatorMachines structs.Machines
+	for _, m := range machines {
+		if m.Operator.Id == operatorId {
+			operatorMachines = append(operatorMachines, m)
+		}
+	}
+
 	return c.Render("app/views/dashboard/list", fiber.Map{
-		"Machines": Machines,
+		"Machines": operatorMachines,
 	})
 }
--- app/store/boltdb/boltdb.go
+++ app/store/boltdb/boltdb.go
@@ -2,6 +2,8 @@ package boltdb
 
 import (
 	"encoding/json"
+	"github.com/typomedia/patchouli/app/helper"
+	"sort"
 	"time"
 
 	"github.com/typomedia/patchouli/app/structs"
@@ -180,6 +182,88 @@ func (bolt *Bolt) GetAllByOperatorId(id string, bucket string) ([][]byte, error)
 		return nil
 	})
 	return result, err
+}
+
+func (bolt *Bolt) GetActiveMachines() (structs.Machines, error) {
+	machines, _ := bolt.GetAll("machine")
+
+	Machines := structs.Machines{}
+	for _, v := range machines {
+		machine := structs.Machine{}
+		err := json.Unmarshal(v, &machine)
+		if err != nil {
+			return nil, err
+		}
+
+		lastUpdate, _ := bolt.GetLastByName(machine.Id, "history")
+
+		if lastUpdate != nil {
+			update := structs.Update{}
+			err = json.Unmarshal(lastUpdate, &update)
+			if err != nil {
+				return nil, err
+			}
+			machine.Update = update
+		}
+
+		if !machine.Inactive {
+			Machines = append(Machines, machine)
+		}
+	}
+	// sort machines by oldest update first
+	sort.Sort(structs.ByDate(Machines))
+
+	config, err := bolt.GetConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range Machines {
+		currentDate := time.Now()
+
+		if Machines[i].Update.Date == "" {
+			Machines[i].Update.Date = "0000-00-00"
+			Machines[i].Status = "danger"
+			continue
+		}
+
+		Machines[i].Update.Date = helper.UnixToDateString(Machines[i].Update.Date)
+
+		date, err := time.Parse(time.DateOnly, Machines[i].Update.Date)
+		if err != nil {
+			return nil, err
+		}
+
+		var interval int
+		// check if machine has custom interval
+		if Machines[i].Interval != 0 {
+			interval = Machines[i].Interval
+		} else {
+			interval = config.General.Interval
+		}
+
+		Machines[i].Days = int(currentDate.Sub(date).Hours() / 24)
+		if Machines[i].Days > interval {
+			Machines[i].Status = "danger"
+		} else if Machines[i].Days > interval/3 {
+			Machines[i].Status = "warning"
+		} else {
+			Machines[i].Status = "success"
+		}
+		Machines[i].Days = interval - int(currentDate.Sub(date).Hours()/24)
+
+	}
+
+	return Machines, nil
+}
+
+func (bolt *Bolt) GetOperatorById(id string) (structs.Operator, error) {
+	var operator structs.Operator
+	err := bolt.Get(id, &operator, "operator")
+	if err != nil {
+		return operator, err
+	}
+	return operator, nil
 }
 
 func (bolt *Bolt) Close() error {
